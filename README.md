646d36971046325ac98609c4740957eb10f86a51
Stefano Fusai <stefanofusai@gmail.com>
2024-07-07 16:59:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# drf-haystack-search-filter
+
+A simple package to implement a [django-haystack](https://github.com/django-haystack/django-haystack) search filter in Django Rest Framework.
+
+## Installation
+
+```bash
+pip install drf-haystack-search-filter
+```
+
+## Usage
+
+Simply import the `HaystackSearchFilter` and use it in your API views:
+
+```python
+from drf_haystack_search_filter.filters import HaystackSearchFilter
+
+...
+
+class MyAPIView(...):
+    ...
+    filter_backends = [HaystackSearchFilter, ...]
+    ...
+```
+
+You can customize the search behavior by overriding the `_search` method.
+
+```python
+from drf_haystack_search_filter.filters import HaystackSearchFilter
+
+class MyHaystackSearchFilter(HaystackSearchFilter):
+    def _search(self, request: Request, queryset: QuerySet[T], view: APIView, query: str) -> QuerySet[T]:
+        # Customize the search behavior here
+        return queryset.filter(
+            pk__in=(
+                SearchQuerySet()
+                .models(queryset.model)
+                .filter(content__startswith=query)
+                .values_list("pk", flat=True)
+            )
+        )
+
+class MyAPIView(...):
+    ...
+    filter_backends = [MyHaystackSearchFilter, ...]
+    ...
+```
+
+## Contributing
+
+Contributions are welcome! To get started, please refer to our [contribution guidelines](https://github.com/stefanofusai/drf-haystack-search-filter/blob/main/CONTRIBUTING.md).
+
+## Issues
+
+If you encounter any problems while using this package, please open a new issue [here](https://github.com/stefanofusai/drf-haystack-search-filter/issues).
---
+++ src/drf_haystack_search_filter/filters.py
@@ -0,0 +1,55 @@
+from typing import TypeVar
+
+from django.db.models import QuerySet
+from haystack.query import SearchQuerySet
+from rest_framework.filters import SearchFilter
+from rest_framework.request import Request
+from rest_framework.views import APIView
+
+T = TypeVar("T")
+
+
+class HaystackSearchFilter(SearchFilter):
+    """A simple search filter for Django Rest Framework that uses Django Haystack's search indexes."""  # noqa: E501
+
+    def filter_queryset(  # noqa: D102
+        self, request: Request, queryset: QuerySet[T], view: APIView
+    ) -> QuerySet[T]:
+        # Avoid search form not being rendered in DRF's Browsable API
+        # (if not getattr(view, 'search_fields', None): return ''),
+        # since HaystackSearchFilter uses Django Haystack's search indexes
+        # rather than standard Django ORM fields.
+        if getattr(view, "search_fields", None) is None:
+            view.search_fields = ["__all__"]  # this can contain anything
+
+        query = request.query_params.get(self.search_param)
+
+        if query is None:
+            return queryset
+
+        return self._search(request, queryset, view, query)
+
+    def _search(
+        self, request: Request, queryset: QuerySet[T], view: APIView, query: str
+    ) -> QuerySet[T]:
+        """A method that can be overridden to customize the search behavior.
+
+        :param request: The request object.
+        :type request: Request
+        :param queryset: The queryset to filter.
+        :type queryset: QuerySet[T]
+        :param view: The view object.
+        :type view: APIView
+        :param query: The search query.
+        :type query: str
+        :return: The filtered queryset.
+        :rtype: QuerySet[T]
+        """  # noqa: D401
+        return queryset.filter(
+            pk__in=(
+                SearchQuerySet()
+                .models(queryset.model)
+                .filter(content__startswith=query)
+                .values_list("pk", flat=True)
+            )
+        )
